1a25aa5f81cf15100137c45a5736ddebed0f57b0
Alexander <alexander.i.kamenev@mail.com>
2017-04-22 00:55:49 +0300

    candidate

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # Ближайшие бары
 
-[FIXME. Здесь будет описание проекта]
+На основании открытых данных о барах можем получить:
+1. самый большой бар;
+2. самый маленький бар;
+3. самый близкий бар;
 
 # Как запустить
 
-Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+# Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
 
 Запуск на Linux:
 
 ```#!bash
 
-$ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
+$ python3 bars.py [get_biggest_bar|get_smallest_bar|get_closest_bar X Y']
+{'ID': '00146638', 'geoData': {'type': 'Point', 'coordinates': [36.900000000253, 55.303299999814]}, 'IsNetObject': '???', 'Latitude_WGS84': '55.3033000000000000', 'District': '?????????????? ?????', 'Longitude_WGS84': '36.9000000000000000', 'AdmArea': '??????????? ???????????????? ?????', 'global_id': 281494712, 'PublicPhone': [{'PublicPhone': '(985) 069-34-47', 'system_object_id': '00146638 _1', 'global_id': 34992.0, 'global_object_id': 281494712.0}], 'Name': 'Staropramen', 'SeatsCount': 50, 'Address': '???????-???????? ?????, ??? 19, ?????? 1', 'system_object_id': '00146638', 'SocialPrivileges': '???', 'TypeObject': '???'}
 
 ```
 
--- bars.py
+++ bars.py
@@ -1,21 +1,69 @@
 import json
+import os
+import sys
+from math import radians, cos, sin, asin, sqrt
 
 
-def load_data(filepath):
-    pass
+BARS = 'bars.json'
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    earth_radius = 6371
+    return c * earth_radius
+
+
+def load_data(file_path=BARS):
+    if not os.path.exists(file_path):
+        return None
+    with open(file_path, 'r', errors='ignore') as file_handler:
+        try:
+            return json.load(file_handler)
+        except json.decoder.JSONDecodeError:
+            return None
 
 
 def get_biggest_bar(data):
-    pass
+    return sorted(data, key=lambda bar: bar['SeatsCount']).pop()
 
 
 def get_smallest_bar(data):
-    pass
+    return sorted(data, key=lambda bar: bar['SeatsCount'], reverse=True).pop()
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    return sorted(data, key=lambda bar: haversine(longitude, latitude,
+                  float(bar['Longitude_WGS84']), float(bar['Latitude_WGS84']))).pop()
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        command = sys.argv[1]
+    except IndexError:
+        print('usage: python3 bars.py [get_biggest_bar|get_smallest_bar|get_closest_bar X Y')
+    else:
+        bars = load_data()
+        if command == 'get_biggest_bar':
+            print(get_biggest_bar(bars))
+        elif command == 'get_smallest_bar':
+            print(get_smallest_bar(bars))
+        elif command == 'get_closest_bar':
+            try:
+                x_coordinate, y_coordinate = float(sys.argv[2]), float(sys.argv[3])
+            except (IndexError, ValueError):
+                print('usage: python3 bars.py [get_biggest_bar|get_smallest_bar|get_closest_bar X Y')
+            else:
+                print(get_closest_bar(bars, x_coordinate, y_coordinate))
+        else:
+            print('usage: python3 bars.py [get_biggest_bar|get_smallest_bar|get_closest_bar X Y')
